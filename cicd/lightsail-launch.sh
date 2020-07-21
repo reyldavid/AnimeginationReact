d2052938dbec0cf972ee0211e0f8a01990fd529d
Rey David <reydavid47@gmail.com>
2020-07-21 16:45:38 -0700

    LIGHTSAIL

---
+++ cicd/lightsail-launch.sh
@@ -0,0 +1,6 @@
+
+curl -o lightsail-compose.sh https://raw.githubusercontent.com/reyldavid/AnimeginationReact/master/cicd/lightsail-compose.sh
+
+chmod +x ./lightsail-compose.sh
+
+./lightsail-compose.sh
