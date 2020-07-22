c107daed10e42e6997c4ec1cd191376e41209f46
Rey David <reydavid47@gmail.com>
2020-07-22 12:14:23 -0700

    lightsail

--- cicd/lightsail-compose.sh
+++ cicd/lightsail-compose.sh
@@ -7,15 +7,19 @@ curl -sSL https://get.docker.com | sh
 usermod -aG docker ubuntu
 
 # install docker-compose
-curl -L https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+curl -L https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 
 # copy the dockerfile into /srv/docker 
 # if you change this, change the systemd service file to match
 # WorkingDirectory=[whatever you have below]
 mkdir /srv/docker
-curl -o /srv/docker/Dockerfile https://raw.githubusercontent.com/reyldavid/AnimeginationReact/master/cicd/Dockerfile
-curl -o /srv/docker/docker-compose.yml https://raw.githubusercontent.com/reyldavid/AnimeginationReact/master/cicd/docker-compose.yml
+cd /srv/docker
+
+sudo git clone https://github.com/reyldavid/AnimeginationReact
+
+curl -o /srv/docker/AnimeginationReact/Dockerfile https://raw.githubusercontent.com/reyldavid/AnimeginationReact/master/cicd/Dockerfile
+curl -o /srv/docker/AnimeginationReact/docker-compose.yml https://raw.githubusercontent.com/reyldavid/AnimeginationReact/master/cicd/docker-compose.yml
 
 # copy in systemd unit file and register it so our compose file runs 
 # on system restart
@@ -23,4 +27,4 @@ curl -o /etc/systemd/system/docker-compose-app.service https://raw.githubusercon
 systemctl enable docker-compose-app
 
 # start up the application via docker-compose
-docker-compose -f /srv/docker/docker-compose.yml up -d
+docker-compose -f /srv/docker/AnimeginationReact/docker-compose.yml up -d
